de7e65f161574e666194bdb0318c720e205b01d7
Andrew <wotriger@gmail.com>
2026-07-18 21:25:30 +0200

    Add variables, icons, images, fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/main.scss'
 
-createRoot(document.getElementById('root')).render(<StrictMode></StrictMode>)
+createRoot(document.getElementById('root')).render(<StrictMode>
+  <h1>Title</h1>
+</StrictMode>)
