d274902d3d115a1247484db53553b54563d46f74
Andrew <wotriger@gmail.com>
2026-07-23 21:08:07 +0200

    add variables and some changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from './components/layout/Footer/Footer'
+import Header from './components/layout/Header/Header'
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	)
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './styles/main.scss'
 
-createRoot(document.getElementById('root')).render(<StrictMode>
-  <h1>Title</h1>
-</StrictMode>)
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+)
